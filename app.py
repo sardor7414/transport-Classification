b746766bbd02ed1e8fdbd813d4ef54bf4d693872
sardor7414 <111555920+sardor7414@users.noreply.github.com>
2023-07-31 12:18:43 +0500

    Fayllar yuklandi

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from fastai.vision.all import *
+import plotly.express as plt
+import pathlib
+
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+st.title('Transportni klassifikatsiya qiluvchi model')
+
+# Rasm yuklash tugmasi
+file = st.file_uploader("Rasmni yuklang", type=['png', 'jpeg', 'jpg', 'svg'])
+
+if file:
+    st.image(file)
+    img = PILImage.create(file)
+    model = load_learner('trasnsport-model.pkl')
+    model.predict(img)
+    pred, pred_id, probs = model.predict(img)
+    st.success(f"Bashorat: {pred}")
+    st.info(f'Ehtimollik: {probs[pred_id]*100:.1f}')
+    fig = plt.bar(x=probs*100, y=model.dls.vocab)
+    st.plotly_chart(fig)
