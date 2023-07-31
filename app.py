5cace16dddba30f794efb70320769366cf0967b3
sardor_mirzaev <111555920+sardor7414@users.noreply.github.com>
2023-07-31 14:58:03 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 from fastai.vision.all import *
 import pathlib
-import plotly.express as px
+import plotly.express as plt
+import platform
 
-plt = platform.system()
-if plt == 'Linux': pathlib.WindowsPath = pathlib.PosixPath
+plf = platform.system()
+if plf == 'Linux': pathlib.WindowsPath = pathlib.PosixPath
 
 st.title('Transportni klassifikatsiya qiluvchi model')
 
@@ -19,5 +20,5 @@ if file:
     pred, pred_id, probs = model.predict(img)
     st.success(f"Bashorat: {pred}")
     st.info(f'Ehtimollik: {probs[pred_id]*100:.1f}')
-    fig = px.bar(x=probs*100, y=model.dls.vocab)
+    fig = plt.bar(x=probs*100, y=model.dls.vocab)
     st.plotly_chart(fig)
